0de15dc0513ff8af186647066910214cd131229c
Alang Kim <rlaangh77@gmail.com>
2025-03-14 13:20:57 +0900

    feat: blog-parser.page UI 개선

--- src/client/pages/home/home.page.tsx
+++ src/client/pages/home/home.page.tsx
@@ -6,8 +6,8 @@ export function HomePage() {
   return (
     <PageLayout>
       <div className="mx-auto w-full max-w-5xl border border-gray-200 rounded-lg p-8">
-        <Typography.H1 className="text-center">Blog To Video</Typography.H1>
-        <div className="my-6">
+        <div className="space-y-8">
+          <Typography.H1 className="text-center">Blog To Video</Typography.H1>
           <BlogUrlForm />
         </div>
       </div>
